7b457e9a8a12b3a665660ca8e532c118aeb2e4e2
andrsand <angorko@list.ru>
2022-05-10 20:30:14 +0300

    next change

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import fs from 'fs';
 
-fs.readFile('readme.txt', 'utf8', function(err, data) {
-	console.log(data);
+fs.readFile('readme1.txt', 'utf8', function(err, data1) {
+	if (!err) {
+		fs.readFile('readme2.txt', 'utf8', function(err, data2) {
+			if (!err) {
+				console.log(data1 * data2);
+			} else {
+				console.log('ошибка чтения файла readme2');
+			}
+		});
+	} else {
+		console.log('ошибка чтения файла readme1');
+	}
 });
