a37e6595290823fd1b705cec252670f5e03681a6
andrsand <angorko@list.ru>
2022-05-20 19:06:45 +0300

    Simple routung

--- HTTPserver/Simple_routing.js
+++ HTTPserver/Simple_routing.js
@@ -3,7 +3,7 @@ import http from 'http';
 http.createServer((request, response) => {
 	if (request.url != '/favicon.ico') {
 		let text;
-		let status = 200;
+		let status = 200;                    // для существующей страницы отдавать статус 200, а для не существующей - 404. 
 		
 		if (request.url == '/page1') {
 			text = '1';
@@ -15,10 +15,12 @@ http.createServer((request, response) => {
 			text = '3';
 		} else {
 			text = 'page not found';
-			status = 404;
+			status = 404;                    // для не существующей страницы - статус - 404. 
 		}
 		
 		response.writeHead(status, {'Content-Type': 'text/html'});
+		response.write(text);
 		response.end();
 	}
 }).listen(3000);
+
