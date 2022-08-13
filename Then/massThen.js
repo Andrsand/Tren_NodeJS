57e67df5feb751dfee75a35b1ec3f9dfb1855e58
andrsand <angorko@list.ru>
2022-08-13 23:32:58 +0300

    docs :  Comment correction
    * Comment correction in Then/massThen.js

--- Then/massThen.js
+++ Then/massThen.js
@@ -7,7 +7,7 @@ for (let name of names) {
 	files.push(fs.promises.readFile(name, 'utf8'));
 }
 
-Promise.all(files).then(data => {                    //Promise.all осуществляет запись в файл только тогда, когда все файлы будут прочитаны.
+Promise.all(files).then(data => {                    //Promise.all only writes to a file when all files have been read.
 	fs.promises.writeFile('res.txt', data.join(''));
 }).catch(err => {
 	console.log('что-то пошло не так');
