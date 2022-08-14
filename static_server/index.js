1f78f95e637fb320faa3695832e7d1e274ce2a7c
andrsand <angorko@list.ru>
2022-08-14 23:46:24 +0300

    docs :  Translation of comments
    * Translation of comments in static_server/index.js

--- static_server/index.js
+++ static_server/index.js
@@ -7,16 +7,16 @@ http.createServer(async (request, response) => {
 	if (request.url != '/favicon.ico') {
 		let text;
 		let status;
-		let path = 'root' + request.url; // путь формируемый из request
+		let path = 'root' + request.url; // path generated from request
 		
 		try {
-			if ((await fs.promises.stat(path)).isDirectory()) { // если не указано имя файла и его расширение				path += '/index.html';
-				path += '/index.html';                          // отправить на главную страницу - index.html
+			if ((await fs.promises.stat(path)).isDirectory()) { // if filename and extension are not specified path += '/index.html';
+				path += '/index.html';                          // send to main page - index.html
 			}
 			
 			status = 200;
-			text = await fs.promises.readFile(path, 'utf8');    // прочитать index.html
-		} catch (err) {                                         //  если ошибка, вернуть статус 404 и вывести соотвествующе сообщение
+			text = await fs.promises.readFile(path, 'utf8');    // read index.html
+		} catch (err) {                                         //  if error, return status 404 and display appropriate message
 			status = 404;
 			text = '404 page not found';
 		}
